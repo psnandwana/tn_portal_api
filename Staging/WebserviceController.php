fb7aa552969b116fc28b99b6dd14e0b1da1ff8b8
psnandwana <pradip.nandwana@indianpac.com>
2020-01-02 19:44:03 +0530

    dd

--- Staging/WebserviceController.php
+++ Staging/WebserviceController.php
@@ -446,6 +446,9 @@ class WebserviceController extends ApistagingController
             if($contact_type == 'Influencer'){
                 $condition['district'] = $district;
                 $User = TableRegistry::get('tn_disctrictwise_influencers');
+            }if($contact_type == 'BLO'){
+                $condition['district'] = $district;
+                $User = TableRegistry::get('tn_disctrictwise_blo');
             }else{
                 if ($searchString != '') {
                     $condition['or']['poc LIKE'] = '%' . $searchString . '%';
